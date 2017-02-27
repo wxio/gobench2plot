89aa60e9d7cd14e6ee10e0395d45db06e42644f2
Gary Miller <miller.garym@gmail.com>
2017-02-27 11:26:43 +1100

    Updated to support b.Run. The output include a / replacing with _ to make it a valid xml element name.

--- gobench2plot.go
+++ gobench2plot.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strings"
 )
 
 func main() {
@@ -12,6 +13,9 @@ func main() {
 	// Examples:
 	// BenchmarkParsePage        100      17788153 ns/op
 	// BenchmarkWrite_4KB_WithIndex       50000         61010 ns/op      67.14 MB/s         598 B/op         16 allocs/op
+	//
+	// using b.Run
+	// BenchmarkQuery/q1.1-6         	   10000	    272723 ns/op
 
 	nsExp := regexp.MustCompile("^(Benchmark.+?)\\s+.*?(\\S+) ns/op.*")
 	allocedBytesExp := regexp.MustCompile("^(Benchmark.+?)\\s+.*?(\\S+) B/op.*")
@@ -27,16 +31,20 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		if tokens := nsExp.FindStringSubmatch(line); tokens != nil {
-			nsMap[tokens[1]] = tokens[2]
+			key := strings.Replace(tokens[1], "/", "_", -1)
+			nsMap[key] = tokens[2]
 		}
 		if tokens := allocedBytesExp.FindStringSubmatch(line); tokens != nil {
-			allocedBytesMap[tokens[1]] = tokens[2]
+			key := strings.Replace(tokens[1], "/", "_", -1)
+			allocedBytesMap[key] = tokens[2]
 		}
 		if tokens := allocedExp.FindStringSubmatch(line); tokens != nil {
-			allocedMap[tokens[1]] = tokens[2]
+			key := strings.Replace(tokens[1], "/", "_", -1)
+			allocedMap[key] = tokens[2]
 		}
 		if tokens := mbExp.FindStringSubmatch(line); tokens != nil {
-			mbMap[tokens[1]] = tokens[2]
+			key := strings.Replace(tokens[1], "/", "_", -1)
+			mbMap[key] = tokens[2]
 		}
 	}
 	fmt.Printf("<Benchmarks>\n")
